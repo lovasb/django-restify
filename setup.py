c374e2178cfbf02f3c318a00d3905ef4eddfe6bc
Schon Peter <slapec@codehouse.hu>
2018-03-06 11:59:12 +0100

    We're django>=2.0 only

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.util import convert_path
 from fnmatch import fnmatchcase
 from setuptools import setup, find_packages
 
-version = '0.33'
+version = '1.0.0'
 
 # Provided as an attribute, so you can append to these instead
 # of replicating them:
